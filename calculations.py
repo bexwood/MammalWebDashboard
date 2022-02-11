1768dfdc5edc2665fd5bb05c97c284a282e8670f
bexwood <bexewood@gmail.com>
2022-02-11 15:06:31 +0000

    kpis 1a and 2b sorted

--- calculations.py
+++ calculations.py
@@ -2,37 +2,68 @@ import json
 import datetime  
 import re
 
+def csProvdingImages(now, year):
+    file = open('data.json')
+    data = json.load(file)
+
+    lastYear=0
+    allTime=0
+    allTimeScientists = []
+    lastYearScientists = []
+
+    for i in data:
+        if i["person_id"] not in allTimeScientists:
+            allTimeScientists.append(i["person_id"])
+            allTime += 1
+
+        test1 = re.compile('.{2}/.{2}/.{4} .{2}:.{2}')
+        test2 = re.compile('.{4}-.{2}-.{2} .{2}:.{2}:.{2}')
+        if test1.match(i["taken"]) is not None:
+            date = datetime.datetime.strptime(i["taken"], '%d/%m/%Y %H:%M') 
+        elif test2.match(i["taken"]) is not None:
+            date = datetime.datetime.strptime(i["taken"], '%Y-%m-%d %H:%M:%S') 
+
+        if i["person_id"] not in lastYearScientists and now-year>date:
+            lastYearScientists.append(i["person_id"])
+            lastYear += 1 
+
+    file.close()
+    return allTime, lastYear
+
+
+def isUploaded(now, year):
+    file = open('data.json')
+    data = json.load(file)
+
+    lastYear=0
+    allTime=0
+
+    for i in data:
+        if i["sequence_num"] == 1:
+            allTime +=1
+
+        test1 = re.compile('.{2}/.{2}/.{4} .{2}:.{2}')
+        test2 = re.compile('.{4}-.{2}-.{2} .{2}:.{2}:.{2}')
+        if test1.match(i["taken"]) is not None:
+            date = datetime.datetime.strptime(i["taken"], '%d/%m/%Y %H:%M') 
+        elif test2.match(i["taken"]) is not None:
+            date = datetime.datetime.strptime(i["taken"], '%Y-%m-%d %H:%M:%S') 
+
+        if i["sequence_num"] == 1 and now-year>date:
+            lastYear += 1 
+        
+
+    file.close()
+    return allTime, lastYear
+
+
+
 now = datetime.datetime.now()
 year = datetime.timedelta(days = 365)
 
-f = open('data.json')
-data = json.load(f)
-lastYear=0
-allTime=0
-allTimeScientists = []
-lastYearScientists = []
-missed =  0
+csProvidingAllTime, csProvidingLastYear = csProvdingImages(now, year)
+isUploadedAllTime, isUploadedLastYear = csProvdingImages(now, year)
 
-for i in data:
-    
-    if i["person_id"] not in allTimeScientists:
-        allTimeScientists.append(i["person_id"])
-        allTime += 1
+print("KPI 1a. All time:",csProvidingAllTime,"Last year:",csProvidingLastYear)
+print("KPI 2b. All time:",isUploadedAllTime,"Last year:",isUploadedLastYear)
 
-    test1 = re.compile('.{2}/.{2}/.{4} .{2}:.{2}')
-    test2 = re.compile('.{4}-.{2}-.{2} .{2}:.{2}:.{2}')
-    if test1.match(i["taken"]) is not None:
-        date = datetime.datetime.strptime(i["taken"], '%d/%m/%Y %H:%M') 
-    elif test2.match(i["taken"]) is not None:
-        date = datetime.datetime.strptime(i["taken"], '%Y-%m-%d %H:%M:%S') 
-    else:
-        missed +=1
-        pass
-    if i["person_id"] not in lastYearScientists and now-year>date:
-        lastYearScientists.append(i["person_id"])
-        lastYear += 1 
-
-print(allTime, lastYear, missed)
-    
-
-f.close()
